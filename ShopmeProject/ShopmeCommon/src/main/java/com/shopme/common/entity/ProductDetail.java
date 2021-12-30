9765c40f473c09ba6a0f38b5b09373fd1ae9fbc8
MinhLe2502 <quangnhatminhle25021998@gmail.com>
2021-12-30 15:44:47 +1100

    Import Sample Data

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/ProductDetail.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/ProductDetail.java
@@ -10,7 +10,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "product_detals")
+@Table(name = "product_details")
 public class ProductDetail {
 	
 	@Id
