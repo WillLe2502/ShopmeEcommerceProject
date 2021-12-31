43ea03322e025e277f032f6f35c9a7f9c1ec79be
MinhLe2502 <quangnhatminhle25021998@gmail.com>
2021-12-31 12:18:10 +1100

    Update Category

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -33,6 +33,9 @@ public class Category {
 	
 	private boolean enabled;
 	
+	@Column(name = "all_parents_ids", length = 256, nullable = true)
+	private String allParentIDs;
+	
 	@OneToOne
 	@JoinColumn(name = "parent_id")
 	private Category parent;
@@ -154,6 +157,14 @@ public class Category {
 		this.children = children;
 	}
 	
+	public String getAllParentIDs() {
+		return allParentIDs;
+	}
+
+	public void setAllParentIDs(String allParentIDs) {
+		this.allParentIDs = allParentIDs;
+	}
+
 	@Transient
 	public String getImagePath() {
 		if(this.id == null) return "/images/image-thumbnail.png";
