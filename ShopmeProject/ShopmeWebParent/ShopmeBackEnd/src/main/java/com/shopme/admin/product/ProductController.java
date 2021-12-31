008c0ea336f12c79d701f62506ea8d2c894d91e2
MinhLe2502 <quangnhatminhle25021998@gmail.com>
2021-12-31 13:44:25 +1100

    Update Product Search Function

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -25,7 +25,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.FileUploadUtil;
 import com.shopme.admin.brand.BrandService;
+import com.shopme.admin.category.CategoryService;
 import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 import com.shopme.common.entity.Product;
 import com.shopme.common.entity.ProductImage;
 
@@ -40,10 +42,13 @@ public class ProductController {
 	@Autowired
 	private BrandService brandService;	
 	
+	@Autowired
+	private CategoryService categoryService;	
+	
 	@GetMapping("/products")
 	public String listFirstPage(Model model) {
 		
-		return listByPage(1, model, "name", "asc", null);
+		return listByPage(1, model, "name", "asc", null, 0);
 	}
 	
 	@GetMapping("/products/pages/{pageNum}")
@@ -51,9 +56,13 @@ public class ProductController {
 							Model model,
 							 @Param("sortField") String sortField,
 							 @Param("sortDir") String sortDir,
-							 @Param("keyword") String keyword) {
-		Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword);
+							 @Param("keyword") String keyword,
+							 @Param("categoryId") Integer categoryId) {
+		
+		Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword, categoryId);
 		List<Product> listProducts = page.getContent();
+		
+		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
 		
 		long startCount = (pageNum - 1) * productService.PRODUCTS_PER_PAGE + 1;
 		long endCount = startCount + productService.PRODUCTS_PER_PAGE - 1;
@@ -62,6 +71,8 @@ public class ProductController {
 		}
 		
 		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+		
+		if(categoryId != null) model.addAttribute("categoryId", categoryId );
 		
 		model.addAttribute("currentPage", pageNum );
 		model.addAttribute("totalPages", page.getTotalPages() );
@@ -73,6 +84,7 @@ public class ProductController {
 		model.addAttribute("reverseSortDir", reverseSortDir );
 		model.addAttribute("keyword", keyword );
 		model.addAttribute("listProducts", listProducts );
+		model.addAttribute("listCategories", listCategories );
 		
 		return "products/products";
 		
--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
@@ -27,7 +27,8 @@ public class ProductService {
 		return (List<Product>) repo.findAll();
 	}
 	
-	public Page<Product> listByPage(int pageNum, String sortField, String sortDir, String keyword){
+	public Page<Product> listByPage(int pageNum, String sortField, String sortDir, 
+									String keyword, Integer categoryId){
 		Sort sort = Sort.by(sortField);
 		
 		if(sortDir.equals("asc")) {
@@ -38,8 +39,18 @@ public class ProductService {
 		
 		Pageable pageable = PageRequest.of(pageNum - 1, PRODUCTS_PER_PAGE, sort);
 		
-		if (keyword != null) {
+		if (keyword != null && !keyword.isEmpty()) {
+			if (categoryId != null && categoryId > 0) {
+				String categoryIdMatch = "-" + String.valueOf(categoryId) + "-";
+				return repo.searchInCategory(categoryId, categoryIdMatch, keyword, pageable);
+			}
+			
 			return repo.findAll(keyword, pageable);
+		}
+		
+		if (categoryId != null && categoryId > 0) {
+			String categoryIdMatch = "-" + String.valueOf(categoryId) + "-";
+			return repo.findAllInCategory(categoryId, categoryIdMatch, pageable);
 		}
 		
 		return repo.findAll(pageable);
