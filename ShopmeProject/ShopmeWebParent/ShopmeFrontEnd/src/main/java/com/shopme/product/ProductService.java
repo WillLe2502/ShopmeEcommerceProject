4cda29311b1dacf471613d61a1bb540dc8df8d32
MinhLe2502 <quangnhatminhle25021998@gmail.com>
2022-01-10 13:53:42 +1100

    Update Search Product UI

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductService.java
@@ -13,6 +13,7 @@ import com.shopme.common.exception.ProductNotFoundException;
 public class ProductService {
 	
 	public static final int PRODUCTS_PER_PAGE = 10;
+	public static final int SEARCH_RESULT_PER_PAGE = 10;
 	
 	@Autowired
 	private ProductRepository repo;
@@ -34,4 +35,9 @@ public class ProductService {
 		return product;
 	}
 	
+	public Page<Product> search(String keyword, int pageNum){
+		Pageable pageable = PageRequest.of(pageNum - 1, SEARCH_RESULT_PER_PAGE);
+		
+		return repo.search(keyword, pageable);
+	}
 }
