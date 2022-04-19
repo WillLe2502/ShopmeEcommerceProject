a830af29658f722c8fc04e34293270a5ccb80a58
MinhLe2502 <quangnhatminhle25021998@gmail.com>
2022-04-19 20:34:32 +1000

    Update Address Functional

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.shopme.common.entity.Address;
+import com.shopme.common.entity.Country;
 import com.shopme.common.entity.Customer;
+import com.shopme.setting.CountryRepository;
 
 @Service
 @Transactional
@@ -16,9 +18,23 @@ public class AddressService {
 	@Autowired
 	private AddressRepository repo;
 	
+	@Autowired
+	private CountryRepository countryRepo;
+	
 	public List<Address> listAddressBook(Customer customer){
 		return repo.findByCustomer(customer);
 	}
 	
+	public void save(Address address) {
+		repo.save(address);
+	}
+	
+	public Address get(Integer addressId, Integer CustomerId) {
+		return repo.findByIdAndCustomer(addressId, CustomerId);
+	}
+	
+	public void delete(Integer addressId, Integer CustomerId) {
+		repo.deleteByIdAndCustomer(addressId, CustomerId);
+	}
 	
 }
